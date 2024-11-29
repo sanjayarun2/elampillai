0e7f74c1fedd4ff7625adbaf502a7d239f855262
sanjayarun2 <eee2015arunkumar.kn15@gmail.com>
2024-11-29 16:47:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# elampillai
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sanjayarun2/elampillai)
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import AdminDashboard from './pages/AdminDashboard';
+import Shops from './pages/Shops';
+import Blog from './pages/Blog';
+import BlogPost from './pages/BlogPost';
+import Marketplace from './pages/Marketplace';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/admin-dashboard" element={<AdminDashboard />} />
+            <Route path="/shops" element={<Shops />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
+            <Route path="/marketplace" element={<Marketplace />} />
+            {/* Catch-all route to handle 404s */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
